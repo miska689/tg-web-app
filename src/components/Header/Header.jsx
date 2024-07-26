b3e917882d9d0acead8b6ee6840ac216853f93d8
miska689 <mihaiboinceanu2004@gmail.com>
2024-07-26 15:26:59 +0300

    bot-menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,4 @@
-import React, {useEffect} from 'react';
-import Button from "../Button/Button";
+import React, {useEffect, useState} from 'react';
 import { FaHome } from "react-icons/fa";
 import { BiTask } from "react-icons/bi";
 import { RiAdminLine } from "react-icons/ri";
@@ -7,12 +6,22 @@ import { IoIosSettings } from "react-icons/io";
 import './Header.css'
 
 const Header = () => {
-    const tg = window.Telegram.WebApp
+    const [selected, setSelected] = useState("home");
+
+    useEffect(() => {
+        const tg = window.Telegram.WebApp
+    }, [])
+
+    useEffect(() => {
+
+    }, []);
 
     const iconSize = 25;
 
-    const onClose = () => {
-        tg.close()
+    const selectItem = (item) => {
+        return function () {
+            setSelected(item);
+        }
     }
 
     return (
@@ -21,29 +30,41 @@ const Header = () => {
                 <hr className={"devider"}/>
                 <div className="nav-menu">
                     <ul className="list-menu">
-                        <li className="list-item">
-                            <div className="icon">
+                        <li
+                            className={"list-item " + (selected === "home" ? "active" : "")}
+                            onClick={selectItem("home")}
+                        >
+                            <div className="icon-fill">
                                 <FaHome className={"icon"} size={iconSize} />
                             </div>
-                            <a href="" className="link-item">Home</a>
+                            <a href="#" className="link-item">Home</a>
                         </li>
-                        <li className="list-item">
-                            <div className="icon">
+                        <li
+                            className={"list-item " + (selected === "regs" ? "active" : "")}
+                            onClick={selectItem("regs")}
+                        >
+                            <div className="icon-fill">
                                 <BiTask className="icon" size={iconSize} />
                             </div>
-                            <a href="" className="link-item">Regs</a>
+                            <a href="#" className="link-item">Regs</a>
                         </li>
-                        <li className="list-item">
-                            <div className="icon">
+                        <li
+                            className={"list-item " + (selected === "set" ? "active" : "")}
+                            onClick={selectItem("set")}
+                        >
+                            <div className="icon-fill">
                                 <IoIosSettings className={"icon"} size={iconSize} />
                             </div>
-                            <a href="" className="link-item">Settings</a>
+                            <a href="#" className="link-item">Settings</a>
                         </li>
-                        <li className="list-item">
-                            <div className="icon">
+                        <li
+                            className={"list-item " + (selected === "admin" ? "active" : "")}
+                            onClick={selectItem("admin")}
+                        >
+                            <div className="icon-fill">
                                 <RiAdminLine className="icon" size={iconSize} />
                             </div>
-                            <a href="" className="link-item">Admin</a>
+                            <a href="#" className="link-item">Admin</a>
                         </li>
                     </ul>
                 </div>
