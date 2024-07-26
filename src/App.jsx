0f42b85eee9519e537213177d41a8bfe24444449
miska689 <mihaiboinceanu2004@gmail.com>
2024-07-26 13:44:23 +0300

    bg-color

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ const App = () => {
             <div className="App">
                 <div className="app-flex">
                     <div className="app-content">
-                        <h1>{tg.themeParams}</h1>
+                        <h1>{tg?.themeParams}</h1>
                     </div>
                     <Header></Header>
                 </div>
