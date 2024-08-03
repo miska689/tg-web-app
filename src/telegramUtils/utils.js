d9ee82127e7eeacfe65464e3f7a842829d450fdf
miska689 <mihaiboinceanu2004@gmail.com>
2024-08-03 15:58:09 +0300

    validate-test

--- src/telegramUtils/utils.js
+++ src/telegramUtils/utils.js
@@ -1,7 +1,7 @@
 import CryptoJS from 'crypto-js';
 
-const generateSecretKey = (telegramBotToken, c_str = "WebAppData") => {
-    return CryptoJS.HmacSHA256(c_str, telegramBotToken).toString();
+const generateSecretKey = (telegramBotToken) => {
+    return CryptoJS.HmacSHA256("WebAppData", telegramBotToken).toString(CryptoJS.enc.Hex);
 };
 
 const validate = (initData, secretKey) => {
@@ -10,10 +10,13 @@ const validate = (initData, secretKey) => {
         .sort()
         .map(key => `${key}=${typeof data[key] === 'object' ? JSON.stringify(data[key]) : data[key]}`)
         .join('\n');
+
+    console.log('Sorted Data:', sortedData); // Verifică sortedData
     const dataCheck = CryptoJS.HmacSHA256(sortedData, secretKey).toString(CryptoJS.enc.Hex);
-    console.log(sortedData);
-    console.log(dataCheck)
-    console.log(hash)
+
+    console.log('Generated Hash:', dataCheck); // Verifică dataCheck
+    console.log('Received Hash:', hash); // Verifică hash-ul primit
+
     return hash === dataCheck;
 };
 
